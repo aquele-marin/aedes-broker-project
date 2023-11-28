3b25a4ed418add6a36c993c8cd3b205d21c2c9a1
Felipe Sampaio <03.felipesampaio@gmail.com>
2023-11-28 01:59:30 -0300

    Add mqtt connection and subscribe to topics

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,62 @@
 const express = require("express");
-require("./src/database/mongoose");
+const http = require('http');
+const mqtt = require('mqtt');
+const websocket = require('websocket-stream');
+const WebSocket = require('ws');
+const { mongoose, writeRoomTemperatureToDatabase, writeDeviceStatusToDatabase } = require("./src/database/connection");
 const Test = require("./src/models/test");
 const cors = require("cors");
 
-const PORT = 3000;
-const HOST = "0.0.0.0";
-
 const app = express();
 app.use(cors());
+
+const server = http.createServer(app); // Used for the web-socket connector
+const wss = new WebSocket.Server({ server });
+
+// Broker connection
+const brokerUrl = 'ws://127.0.0.1:8884';
+const topics = ['room_temperature', 'device_status'];  // Topics to subscribe
+const client = mqtt.connect(brokerUrl, {
+  clientId: 'node_backend',
+  username: 'asd',
+  password: 'asdasd'
+});
+
+client.subscribe(topics, (err, granted) => {
+  if (err) {
+      console.error('Error subscribing to topics:', err);
+  } else {
+      console.log('Subscribed to topics:', granted);
+  }
+});
+
+client.on('message', async (receivedTopic, message) => {
+  if (receivedTopic === 'room_temperature'){
+    jsonMessage = JSON.parse(message.toString())
+    await writeRoomTemperatureToDatabase(jsonMessage.current_time, jsonMessage.device_id, jsonMessage.room_temperature);
+  }
+  console.log(JSON.parse(message.toString()))
+});
+
+// NAO FUNCIONAAAAAA
+// console.log('Before websocket.createServer');
+// websocket.createServer({ server }, (stream, request) => {
+//   console.log("Nem entrou")
+//   const duplexStream = websocket.createWebSocketStream(stream);
+//   client.pipe(duplexStream).pipe(client);
+
+//   client.subscribe(["room_temperature", "device_status"], (err, granted) => {
+//     if (err) {
+//         console.error('Error subscribing to topics:', err);
+//     } else {
+//         console.log('Subscribed to topics:', granted);
+//     }
+//   });
+
+//   client.on('message', async (receivedTopic, message) => {
+//     console.log(message)
+//   });
+// });
 
 // API
 app.get("/", async (req, res) => {
@@ -19,4 +68,7 @@ app.get("/", async (req, res) => {
   return res.send(test);
 });
 
-app.listen(PORT, HOST);
+// app.listen(3000, "127.0.0.1");
+server.listen(3000, () => {
+  console.log(`Server listening on port ${3000}`);
+});
