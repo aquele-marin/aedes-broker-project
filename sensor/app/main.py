601004d2b093bb698e3e33a7167c73c0972fec7f
Felipe Sampaio <03.felipesampaio@gmail.com>
2023-11-28 00:27:36 -0300

    Add error to when device is not on

--- sensor/app/main.py
+++ sensor/app/main.py
@@ -1,64 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from contextlib import asynccontextmanager
-import asyncio
-import threading
-import time
 
-from .air_conditioning import AirConditioning
+from .air_conditioning import AirConditioning, DeviceIsNotTurnedOnException
 from . import schemas
 
-run_enviroment_temp_calculation = True
-run_send_environment_temp_to_topic = True
-a = 1
 
 device = AirConditioning()
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # print('Before Geral:', AirConditioning.ENV_TEMP_CALCULATION)
-    # print('Before Device:', device.ENV_TEMP_CALCULATION)
     yield
-    device.ENV_TEMP_CALCULATION = False
-    # print('Before Geral:', AirConditioning.ENV_TEMP_CALCULATION)
-    # print('Before Device:', device.ENV_TEMP_CALCULATION)
+    device.kill()
 
 
 app = FastAPI(lifespan=lifespan)
-
-
-
-def write_result_to_file(result):
-    # Write the result to a file
-    # with open("result.txt", "a") as file:
-    #     file.write(f"{result}\n")
-    print(result)
-
-
-# def background_task():
-#     global run_enviroment_temp_calculation
-#     while run_enviroment_temp_calculation:
-#         # Your task logic goes here
-#         result = device.environment_temperature
-
-#         # Write the result to a file
-#         write_result_to_file(result)
-
-#         # Sleep for one second
-#         time.sleep(1)
-
-
-# background_thread_temp_calculation = threading.Thread(target=background_task)
-# background_thread_temp_calculation.start()
-
-# RETIRAR ISSO DEPOIS
-device.turn_on()
-
-# @asyncio.coroutine
-def change_environment_temperature():
-    new_temp = device.calculate_new_environment_temperature()
-    device.environment_temperature = new_temp
-    print('Env', str(device.environment_temperature))
-    return device.environment_temperature
 
 
 @app.get('/')
@@ -80,7 +36,10 @@ async def turn_off_device(id: str):
 
 @app.post('/devices/{id}/temperature', status_code=200, response_model=schemas.DeviceStatus)
 async def set_new_temperature_for_device(id: str, new_temperature: int):
-    device.set_temperature(new_temperature)
+    try:
+        device.set_temperature(new_temperature)
+    except DeviceIsNotTurnedOnException as e:
+        raise HTTPException(409, str(e))
     return device
 
 
@@ -88,7 +47,10 @@ async def set_new_temperature_for_device(id: str, new_temperature: int):
 async def increase_temperature(id: str, value: int = 1):
     if value < 0:
         raise HTTPException(422, f'Value must be above 0')
-    device.set_temperature(device.temperature + value)
+    try:
+        device.set_temperature(device.temperature + value)
+    except DeviceIsNotTurnedOnException as e:
+        raise HTTPException(409, str(e))
     return device
 
 
@@ -96,7 +58,10 @@ async def increase_temperature(id: str, value: int = 1):
 async def decrease_temperature(id: str, value: int = 1):
     if value < 0:
         raise HTTPException(422, f'Value must be above 0')
-    device.set_temperature(device.temperature - value)
+    try:
+        device.set_temperature(device.temperature - value)
+    except DeviceIsNotTurnedOnException as e:
+        raise HTTPException(409, str(e))
     return device
 
 
@@ -104,7 +69,10 @@ async def decrease_temperature(id: str, value: int = 1):
 async def increase_intensity(id: str, value: int = 1):
     if value < 0:
         raise HTTPException(422, f'Value must be above 0')
-    device.set_intensity(device.intensity + value)
+    try:
+        device.set_intensity(device.intensity + value)
+    except DeviceIsNotTurnedOnException as e:
+        raise HTTPException(409, str(e))
     return device
 
 
@@ -112,13 +80,8 @@ async def increase_intensity(id: str, value: int = 1):
 async def decrease_intensity(id: str, value: int = 1):
     if value < 0:
         raise HTTPException(422, f'Value must be above 0')
-    device.set_intensity(device.intensity - value)
-    return device
-
-
-@app.get('/devices/{id}/environment', status_code=200, response_model=schemas.EnvironmentTemp)
-async def get_new_environment_tempearute(id: str):
-    new_temp = device.calculate_new_environment_temperature()
-    device.environment_temperature = new_temp
-    # device.change_environment_temperature(new_temp + device.environment_temperature)
+    try:
+        device.set_intensity(device.intensity - value)
+    except DeviceIsNotTurnedOnException as e:
+        raise HTTPException(409, str(e))
     return device
